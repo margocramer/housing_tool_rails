1c8eaea82d4ca66b712b0e8817345e0cdba680a0
Marguerite Cramer <margueritecramer@Marguerites-MacBook-Air.local>
2017-08-11 15:10:27 -0400

    remove comments

--- app/controllers/api/v1/household_income_controller.rb
+++ app/controllers/api/v1/household_income_controller.rb
@@ -7,17 +7,7 @@ class Api::V1::HouseholdIncomeController < ApiController
   end
 
   def create
-    
+
   end
 
 end
-
-# @household_income = { household_income: HouseholdIncome.where("income_max = ?", params[:income]) }
-# @unit_rents = { unit_rent: UnitRent.where("household_income = ? AND bedrooms = ?", params[:household_income], params[:bedrooms]) }
-# household_income_unit_rent = @household_income.merge(@unit_rent)
-# render json: household_income_unit_rent, adapter: :json
-
-# @household_income = { household_income: HouseholdIncome.all }
-# @unit_rents = { unit_rent: UnitRent.all }
-# @household_income_unit_rent = @household_income.merge(@unit_rent)
-# render json: @household_income_unit_rent, adapter: :json
